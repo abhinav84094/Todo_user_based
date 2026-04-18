959f44fa3c4172631efbfe257a4a48c6a468a05f
abhinav84094 <abhinav84094@gmail.com>
2026-04-18 22:40:28 +0530

    todo backend implementation

---
+++ backend/index.js
@@ -0,0 +1,44 @@
+import express from "express"
+
+const app = express();
+app.use(express.json())
+
+let todos = []
+let todoId = 1
+
+
+app.get("/", function home(req, res){
+    res.sendFile("A:\\FULL STACK Web Development MERN\\Project\\2026\\NotesApp\\frontend\\home.html");
+})
+
+app.post("/todos", function(req, res){
+    let todo = req.body.todo; 
+    todos.push({
+        todo,
+        todoId: todoId++
+    });
+    res.status(200).json({
+        messages: todo
+    })
+})
+
+app.get("/todos", function(req, res){
+    res.send(todos)
+})
+
+app.delete("/todos/:id", function(req, res){
+    const id = req.params.id;
+
+    todos = todos.filter(todo=> todo.todoId!=id)
+    console.log(todos)
+    
+    res.status(202).json(
+        {
+            messages: `${id} deleted`
+        }
+    )
+
+})
+
+
+app.listen(3000)
